7e8c0240e29d2080f96909f38136bc298e85f46f
mengqt <1471385100@qq.com>
2024-12-03 14:28:26 +0800

    code format

--- src/index.js
+++ src/index.js
@@ -13,31 +13,6 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log(username);
 });
 
-// document.getElementById("breakfast-button").addEventListener("click", function () {
-//     document.getElementById('breakfast-input').click();
-// });
-//
-// document.getElementById("lunch-button").addEventListener("click", function () {
-//     document.getElementById('lunch-input').click();
-// });
-//
-// document.getElementById("dinner-button").addEventListener("click", function () {
-//     document.getElementById('dinner-input').click();
-// });
-
-// document.getElementById("breakfast-calibration").addEventListener("click", function () {
-//     breakfastCalibration();
-// });
-//
-// document.getElementById("lunch-calibration").addEventListener("click", function () {
-//     lunchCalibration();
-// });
-//
-// document.getElementById("dinner-calibration").addEventListener("click", function () {
-//     dinnerCalibration();
-// });
-
-
 function bindButtonToInputClick(buttonId, inputId) {
     const button = document.getElementById(buttonId);
     if (button) {
@@ -60,114 +35,24 @@ function bindCalibrationButtonClick(calibrationButtonId, calibrationFunction) {
 }
 
 const buttonInputPairs = [
-    { buttonId: "breakfast-button", inputId: "breakfast-input" },
-    { buttonId: "lunch-button", inputId: "lunch-input" },
-    { buttonId: "dinner-button", inputId: "dinner-input" }
+    {buttonId: "breakfast-button", inputId: "breakfast-input"},
+    {buttonId: "lunch-button", inputId: "lunch-input"},
+    {buttonId: "dinner-button", inputId: "dinner-input"}
 ];
 
-const calibrationFunctions = {
-    "breakfast-calibration": breakfastCalibration,
-    "lunch-calibration": lunchCalibration,
-    "dinner-calibration": dinnerCalibration
-};
+const calibrationFunctions = [
+    {calibrationButtonId: "breakfast-calibration", calibrationFunction: breakfastCalibration},
+    {calibrationButtonId: "lunch-button", calibrationFunction: lunchCalibration},
+    {calibrationButtonId: "dinner-button", calibrationFunction: dinnerCalibration},
+];
 
-buttonInputPairs.forEach(({ buttonId, inputId }) => {
+buttonInputPairs.forEach(({buttonId, inputId}) => {
     bindButtonToInputClick(buttonId, inputId);
 });
 
 calibrationFunctions.forEach((calibrationButtonId, calibrationFunction) => {
     bindCalibrationButtonClick(calibrationButtonId, calibrationFunction);
 });
-
-
-
-// function breakfastCalibration() {
-//
-//     const username = getUsernameFromCookie();
-//     const date = document.getElementById('date').value;
-//     const change = document.getElementById('breakfast-recognition').value;
-//
-//     const xhr = new XMLHttpRequest();
-//     xhr.open('POST', 'http://10.189.140.61:18080/config_breakfast', true);
-//     xhr.setRequestHeader('Content-Type', 'application/json');
-//
-//     const data = {
-//         user_name: username,
-//         date: date,
-//         change: change,
-//     };
-//
-//     const jsonData = JSON.stringify(data);
-//
-//     xhr.send(jsonData);
-//
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState === 4 && xhr.status === 200) {
-//             console.log('请求成功，响应内容：', xhr.responseText);
-//             document.getElementById('breakfast-recognition').innerHTML = xhr.responseText;
-//         } else if (xhr.readyState === 4) {
-//             console.log('请求失败，状态码：', xhr.status);
-//         }
-//     };
-// }
-//
-// function lunchCalibration() {
-//     const username = getUsernameFromCookie();
-//     const date = document.getElementById('date').value;
-//     const change = document.getElementById('lunch-recognition').value;
-//
-//     const xhr = new XMLHttpRequest();
-//     xhr.open('POST', 'http://10.189.140.61:18080/config_lunch', true);
-//     xhr.setRequestHeader('Content-Type', 'application/json');
-//
-//     const data = {
-//         user_name: username,
-//         date: date,
-//         change: change,
-//     };
-//
-//     const jsonData = JSON.stringify(data);
-//
-//     xhr.send(jsonData);
-//
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState === 4 && xhr.status === 200) {
-//             console.log('请求成功，响应内容：', xhr.responseText);
-//             document.getElementById('lunch-recognition').innerHTML = xhr.responseText;
-//         } else if (xhr.readyState === 4) {
-//             console.log('请求失败，状态码：', xhr.status);
-//         }
-//     };
-// }
-//
-// function dinnerCalibration() {
-//     const username = getUsernameFromCookie();
-//     const date = document.getElementById('date').value;
-//     const change = document.getElementById('dinner-recognition').value;
-//
-//     const xhr = new XMLHttpRequest();
-//     xhr.open('POST', 'http://10.189.140.61:18080/config_dinner', true);
-//     xhr.setRequestHeader('Content-Type', 'application/json');
-//
-//     const data = {
-//         user_name: username,
-//         date: date,
-//         change: change,
-//     };
-//
-//     const jsonData = JSON.stringify(data);
-//
-//     xhr.send(jsonData);
-//
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState === 4 && xhr.status === 200) {
-//             console.log('请求成功，响应内容：', xhr.responseText);
-//             document.getElementById('dinner-recognition').innerHTML = xhr.responseText;
-//         } else if (xhr.readyState === 4) {
-//             console.log('请求失败，状态码：', xhr.status);
-//         }
-//     };
-// }
 
 function makeCalibrationRequest(endpoint, recognitionId) {
     const username = getUsernameFromCookie();
@@ -197,9 +82,11 @@ function makeCalibrationRequest(endpoint, recognitionId) {
 function breakfastCalibration() {
     makeCalibrationRequest('http://10.189.140.61:18080/config_breakfast', 'breakfast-recognition');
 }
+
 function lunchCalibration() {
     makeCalibrationRequest('http://10.189.140.61:18080/config_lunch', 'lunch-recognition');
 }
+
 function dinnerCalibration() {
     makeCalibrationRequest('http://10.189.140.61:18080/config_dinner', 'dinner-recognition');
 }
