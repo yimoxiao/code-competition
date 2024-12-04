c49d2b2ec8fc1ab46acf1d877988ce452c6cfb9c
lorreshi <lorre_shi@163.com>
2024-12-05 00:10:25 +0800

    Output format add markdown
    Signed-off-by: lorreshi <lorre_shi@163.com>

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ document.addEventListener('DOMContentLoaded', function () {
     // 从localStorage中获取用户名
     var username = getUsernameFromCookie();
 
-    if (!username) {
-        alert("请先登录");
-        window.location.href = "login.html";
-        return;
-    }
-    console.log(username);
+    // if (!username) {
+    //     alert("请先登录");
+    //     window.location.href = "login.html";
+    //     return;
+    // }
+    // console.log(username);
 });
 
 function bindButtonToInputClick(buttonId, inputId) {
--- src/textDisplay.js
+++ src/textDisplay.js
@@ -1,14 +1,16 @@
 // 用于逐字显示文本的函数
 function typeText(elementId, text, speed) {
     const element = document.getElementById(elementId);
-    element.textContent = ""; // 清空之前的内容
+    element.innerHTML  = ""; // 清空之前的内容
     let index = 0;
 
     function type() {
         if (index < text.length) {
-            element.textContent += text[index];
+            // 解析为md格式
+            const parsedText = marked(text.slice(0, index + 1));
+            element.innerHTML = parsedText;
             index++;
-            setTimeout(type, speed); // 递归调用，逐字显示
+            setTimeout(type, speed);
         }
     }
 
